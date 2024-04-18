eefeba7c74c4758eeab0c5254608afa97a8bbd42
Thanuga Soysa <thanugasoysa2003@gmail.com>
2024-04-18 18:40:43 +0530

    aboutUs page done

---
+++ js/pageLoader.js
@@ -0,0 +1,25 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Get the button element
+    const loadbasicChilliPage = document.getElementById("basicChilliPackage");
+
+    // Add click event listener to the button
+    loadbasicChilliPage.addEventListener("click", function() {
+        // Redirect to new page
+        window.location.href = "basicChilliPacakage.html";
+    });
+    
+    
+
+    
+
+    
+
+    
+
+    
+
+
+
+});
+
+
